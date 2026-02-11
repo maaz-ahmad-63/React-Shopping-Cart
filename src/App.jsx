4974d7f99589639925a7759bb7d9c32218e99ff6
maaz-ahmad-63 <maazahmad6388@gmail.com>
2026-02-11 11:13:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import ProductList from './Pages/ProductList'
+import Cart from './Pages/Cart'
+import Checkout from './Pages/Checkout'
+import ProductDetail from './Pages/productDetail'
+import Footer from './components/Footer'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { ToastContainer, toast,Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const App = () => {
+  return (
+    <>
+    
+      <Router>
+        <ToastContainer
+          position="top-right"
+          autoClose={1500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+          transition={Bounce}
+        />
+        <div className="min-h-screen bg-gray-950 font-sans">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/checkout" element={<Checkout />} />
+          </Routes>
+          <Footer />
+        </div>
+      </Router>
+    </>
+  );
+};
+
+
+export default App
+console.log("App is rendering");
